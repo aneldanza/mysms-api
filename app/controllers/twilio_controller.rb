68b7736bb3a4a7734038d6268768c738fe491dbb
Anel Danza <aneldanza23@gmail.com>
2025-04-25 02:58:19 -0400

    broadcast message status when update is received from twilio

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,9 +1,20 @@
 class TwilioController < ApplicationController
+  protect_from_forgery with: null_session
+
   def status
     sid = params[:MessageSid]
     status = params[:MessageStatus]
 
     message = Message.find_by(twilio_sid: sid)
+
+    # Broadcast the new status
+    ActionCable.server.broadcast(
+      "message_status",
+      {
+        id: message.id.to_s,
+        status: message.status
+      }
+    )
 
     if message
       message.update(status: status)
