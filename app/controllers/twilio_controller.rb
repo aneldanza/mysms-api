1a1b744111ded18baa2ffed722dbec864148f410
Anel Danza <aneldanza23@gmail.com>
2025-04-25 03:41:35 -0400

    fix error

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -7,17 +7,17 @@ class TwilioController < ApplicationController
 
     message = Message.find_by(twilio_sid: sid)
 
-    # Broadcast the new status
-    ActionCable.server.broadcast(
-      "message_status",
-      {
-        id: message.id.to_s,
-        status: message.status
-      }
-    )
-
+    
     if message
       message.update(status: status)
+      # Broadcast the new status
+      ActionCable.server.broadcast(
+        "message_status",
+        {
+          id: message.id.to_s,
+          status: message.status
+        }
+      )
       head :ok
     else
       Rails.logger.error("Message with SID #{sid} not found.")
