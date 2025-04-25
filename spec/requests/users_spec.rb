8f1892268f8ca2175c769793569275ba53048ecd
Anel Danza <aneldanza23@gmail.com>
2025-04-25 10:34:57 -0400

    update SSL

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET /me' do
+    let(:user) { User.create!(email: 'test@example.com', username: 'tester', password: 'password') }
+
+    context 'when authenticated' do
+      let(:token) do
+        Warden::JWTAuth::UserEncoder.new.call(user, :user, nil).first
+      end
+
+      it 'returns the current user data' do
+        get '/me', headers: { 'Authorization' => "Bearer #{token}" }
+
+        expect(response).to have_http_status(:ok)
+        json = JSON.parse(response.body)
+
+        expect(json['id']).to eq(user.id.to_s)
+        expect(json['email']).to eq(user.email)
+        expect(json['username']).to eq(user.username)
+        expect(json).to have_key('created_at')
+        expect(json).to have_key('updated_at')
+      end
+    end
+
+    context 'when not authenticated' do
+      it 'returns unauthorized' do
+        get '/me'
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
+end
