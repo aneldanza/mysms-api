35924e72af0d98ae250e0e0dc339225ea525681d
Anel Danza <aneldanza23@gmail.com>
2025-04-24 21:17:43 -0400

    fix error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,9 @@ class UsersController < ApplicationController
 
   def me
     render json: {
-        id: current_user.id.to_s,
+        id: current_user.id,
         email: current_user.email,
-        name: current_user.name,
+        username: current_user.username,
         created_at: current_user.created_at,
         updated_at: current_user.updated_at,
     }
