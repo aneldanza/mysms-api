a754b50bad50e69cb91503aa1bd6aa70db79b4dd
Anel Danza <aneldanza23@gmail.com>
2025-04-25 07:19:49 -0400

    enable SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
-  config.action_cable.allowed_request_origins = [ "https://mysms-frontend-git-main-aneldanzas-projects.vercel.app" ]
+  config.action_cable.allowed_request_origins = [ "https://mysms-frontend-git-main-aneldanzas-projects.vercel.app", "https://sms-messenger-api-9c3caa66eb6a.herokuapp.com" ]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
