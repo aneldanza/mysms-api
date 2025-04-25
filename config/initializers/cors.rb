854d19091e0beb37d191919ab03b75707c26a87e
Anel Danza <aneldanza23@gmail.com>
2025-04-25 04:46:18 -0400

    add prod url of front end

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:4200"
+    origins ["http://localhost:4200", "https://mysms-frontend-git-main-aneldanzas-projects.vercel.app"]
 
     resource "*",
       headers: :any,
