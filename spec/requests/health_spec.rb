b873c91dcf83c2938cb63b8bd371199cecee950b
Anel Danza <aneldanza23@gmail.com>
2025-04-26 18:15:45 -0400

    update health controller test

--- spec/requests/health_spec.rb
+++ spec/requests/health_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Healths", type: :request do
   describe "GET /index" do
-    it "returns http success" do
-      get "/health/index"
-      expect(response).to have_http_status(:success)
+    it "returns http ok" do
+      get "/health"
+      expect(response).to have_http_status(:ok)
     end
   end
 
